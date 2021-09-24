a142e03b3843bffee2d2f1ed3af4ab9ede195184
Igor Losev <igor.losev@gmail.com>
2021-09-24 18:28:20 +0700

    [FEATURE] Add common selectors: getAll, getById, getStatus.

--- README.md
+++ README.md
@@ -2,3 +2,62 @@
 Wrapper for Redux Slice with CRUD operations over Rest API.
 
 > The library is in development so interfaces may change: do not use in production!
+
+## Usage
+
+```javascript
+import {
+    createSlice,
+    // import some constants if needed 
+    STATUS_LOADING,
+    STATUS_OK,
+    STATUS_ERROR_AUTH,
+    STATUS_ERROR_READ,
+    STATUS_ERROR_WRITE,
+    STATUS_ERROR_UNKNOWN,
+    ERRORS_STATUSES,
+} from '@hbsolutions/redux-rest-crud';
+
+export const {
+    slice: myEntitySlice,
+    // extract the selectors you need
+    selectors: { getAll, getById, getStatus }
+} = createSlice('myEntities');
+
+// extract the necessary actions for use in thunks or elsewhere
+export const { startLoading, setItems, setError, addItem, editItem, deleteItem } = myEntitySlice.actions;
+
+// export reducer to configure the store
+export default myEntitySlice.reducer;
+```
+
+Thunk example:
+```javascript
+export const loadMyEntities = () => {
+    return async (dispatch, getState) => {
+        const state = getState();
+
+        if (state.myEntities.status === STATUS_LOADING) {
+            return;
+        }
+
+        dispatch(startLoading());
+
+        // Check auth if needed
+        if (!isAuthSuccess()) {
+            dispatch(setError(STATUS_ERROR_AUTH));
+            return;
+        }
+
+        try {
+            const data = await fetchDataFromTheServer();
+            dispatch(setItems({
+                data,
+                deps: null,
+            }));
+        } catch (error) {
+            dispatch(setError(STATUS_ERROR_READ));
+        }
+    };
+};
+```
--- src/createSlice.js
+++ src/createSlice.js
@@ -5,43 +5,59 @@ const createSlice = (name, insertDirection, dependenciesFunction) => {
     insertDirection = insertDirection || C.INSERT_DIRECTION_BOTTOM;
     dependenciesFunction = dependenciesFunction || (deps => JSON.stringify(deps));
 
-    return toolkitCreateSlice({
-        name,
-        initialState: {
-            items: [],
-            dependencies: null,
-            status: C.STATUS_INIT,
+    return {
+        slice: toolkitCreateSlice({
+            name,
+            initialState: {
+                items: [],
+                dependencies: null,
+                status: C.STATUS_INIT,
+            },
+            reducers: {
+                startLoading: state => {
+                    state.status = C.STATUS_LOADING;
+                },
+                setError: (state, { payload }) => {
+                    state.status = C.ERRORS_STATUSES.indexOf(payload) !== -1 ? payload : C.STATUS_ERROR_UNKNOWN;
+                },
+                setItems: (state, { payload }) => {
+                    state.items = payload.data;
+                    state.dependencies = dependenciesFunction(payload.deps);
+                    state.status = C.STATUS_OK;
+                },
+                addItem: (state, { payload }) => {
+                    if (insertDirection === C.INSERT_DIRECTION_TOP) {
+                        state.items.unshift(payload);
+                    } else {
+                        state.items.push(payload);
+                    }
+                    state.status = C.STATUS_OK;
+                },
+                editItem: (state, { payload }) => {
+                    const index = state.items.findIndex(item => item.id === payload.id);
+                    index > -1 && (state.items[index] = payload);
+                },
+                deleteItem: (state, { payload }) => {
+                    const index = state.items.findIndex(item => item.id === payload);
+                    index > -1 && state.items.splice(index, 1);
+                },
+            },
+        }),
+
+        selectors: {
+            getAll: state => state[name].items,
+
+            getById: id => state => (
+                state[name].items.find(obj => obj.id === id.toString())
+            ),
+
+            getStatus: state => ({
+                hasError: C.ERRORS_STATUSES.indexOf(state[name].status) !== -1,
+                isLoading: state[name].status === C.STATUS_LOADING,
+                isReady: state[name].status === C.STATUS_OK,
+            }),
         },
-        reducers: {
-            startLoading: state => {
-                state.status = C.STATUS_LOADING;
-            },
-            setError: (state, { payload }) => {
-                state.status = C.ERRORS_STATUSES.indexOf(payload) !== -1 ? payload : C.STATUS_ERROR_UNKNOWN;
-            },
-            setItems: (state, { payload }) => {
-                state.items = payload.data;
-                state.dependencies = dependenciesFunction(payload.deps);
-                state.status = C.STATUS_OK;
-            },
-            addItem: (state, { payload }) => {
-                if (insertDirection === C.INSERT_DIRECTION_TOP) {
-                    state.items.unshift(payload);
-                } else {
-                    state.items.push(payload);
-                }
-                state.status = C.STATUS_OK;
-            },
-            editItem: (state, { payload }) => {
-                const index = state.items.findIndex(item => item.id === payload.id);
-                index > -1 && (state.items[index] = payload);
-            },
-            deleteItem: (state, { payload }) => {
-                const index = state.items.findIndex(item => item.id === payload);
-                index > -1 && state.items.splice(index, 1);
-            },
-        },
-    });
+    };
 };
 
 export default createSlice;
